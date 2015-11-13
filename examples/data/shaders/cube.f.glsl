cb53ee0472ce990f9c9df877fce62214cb201ad9
Ed Ye <hahahadude@gmail.com>
2015-11-13 06:00:12 -0500

    working on flocking and added data files

---
+++ examples/data/shaders/cube.f.glsl
@@ -0,0 +1,11 @@
+#version 330
+
+//in vec2 Texcoord;
+
+out vec4 outColor;
+
+uniform vec4 color;
+
+void main() {
+  outColor = color;
+}
--- examples/flock.lisp
+++ examples/flock.lisp
@@ -1,6 +1,23 @@
 ;;; flocking behaviour
 
-(defun flock-init ())
+#|
+1. seperation
+2. alignment
+3. cohesion
+|#
+
+(defun init-managers ()
+  (setf *program-manager* (make-instance 'program-manager)
+        *texture-manager* (make-instance 'texture-manager)
+        *font-manager* (make-instance 'font-manager)))
+(defun flock-init ()
+  (let* ((proj-dir (asdf:system-source-directory :err-examples))
+         (shader-dir (merge-pathnames #p"examples/data/shaders/" proj-dir))
+         (text-program (make-program (merge-pathnames #p"text.v.glsl" shader-dir)
+                                     (merge-pathnames #p"text.f.glsl" shader-dir)))
+         (cube-program (make-program (merge-pathnames #p"cube.v.glsl" shader-dir)
+                                     (merge-pathnames #p"cube.f.glsl" shader-dir))))
+    ))
 (defun flock-handle-input ())
 (defun flock-render ())
 (defun flock-update ())
