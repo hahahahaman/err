719995ad420d018701336ae600e670507cb514da
Ed Ye <hahahadude@gmail.com>
2016-01-04 19:36:23 -0500

    Added a model matrix to text drawing shader. Still working on getting draw-center and rotation-center parameters to work with text-draw.

--- src/text-drawer.lisp
+++ src/text-drawer.lisp
@@ -24,75 +24,112 @@
                     (rotate 0.0)
                     (rotation-center (vec3f 0.0 0.0 0.0))
                     (draw-center (vec3f 0.0 0.0 0.0))
-                    (draw-mode :triangles)
                     (drawer *text-drawer*))
   "=> TEXT-WIDTH, TEXT-HEIGHT
 Draws a text string on screen."
   (with-slots (program vao vbo) drawer
     (gl:use-program (id program))
     (gl:uniformfv (get-uniform program "textColor") color)
+    (let ((model (kit.glm:matrix*
+                  ;; order of operations is bottom to top since
+                  ;; matrix multiplication
+
+                  ;;finally move to POSITION
+                  (kit.glm:translate position)
+
+                  ;; move to draw center
+                  (kit.glm:translate* (cfloat (* (x-val draw-center)
+                                                 (x-val scale)))
+                                      (cfloat (* (y-val draw-center)
+                                                 (y-val scale)))
+                                      (cfloat (* (z-val draw-center)
+                                                 1.0)))
+
+                  ;; move back from rotation center
+                  (kit.glm:translate* (cfloat (* (x-val rotation-center)
+                                                 (x-val scale)))
+                                      (cfloat (* (y-val rotation-center)
+                                                 (y-val scale)))
+                                      (cfloat (* (z-val rotation-center)
+                                                 1.0)))
+                  ;; ;; rotate around the z-axis
+                  (kit.glm:rotate* 0.0 0.0 (cfloat rotate))
+                  ;; move to rotation center
+                  (kit.glm:translate* (cfloat (* -1.0
+                                                 (x-val rotation-center)
+                                                 (x-val scale)))
+                                      (cfloat (* -1.0
+                                                 (y-val rotation-center)
+                                                 (y-val scale)))
+                                      (cfloat (* -1.0
+                                                 (z-val rotation-center)
+                                                 1.0)))
+
+                  ;; scale first, z axis remain constant since 2d
+                  (kit.glm:scale* (cfloat (x-val scale)) (cfloat (y-val scale)) 0.0))))
+
+      ;; set model uniform
+      (gl:uniform-matrix-4fv (get-uniform program "model") (vector model) nil))
+
     (gl:bind-vertex-array vao)
-    (let ((total-width 0.0)
-          (lowest-y 0.0)
-          (highest-y 0.0))
-      ;; iteratively render all characters of TEXT
-      (iter (for c in-vector text)
-        (let* ((tc (@ font-text-chars c)) ;; text-char
-               (text-char-bearing (text-char-bearing tc))
-               (text-char-size (text-char-size tc))
-               (text-char-advance (text-char-advance tc))
-               (tc-bearing (vec2f-mul (vec2f (cfloat (x-val text-char-bearing))
-                                             (cfloat (y-val text-char-bearing)))
-                                      scale))
-               (tc-size (vec2f-mul (vec2f (cfloat (x-val text-char-size))
-                                          (cfloat (y-val text-char-size)))
-                                   scale))
-               (tc-advance (* (x-val scale) text-char-advance))
-               (xpos (+ (x-val position)
-                        (x-val tc-bearing)))
-               (ypos (+ (y-val position)
-                        (y-val tc-bearing)))
-               (w (x-val tc-size))
-               (h (y-val tc-size)))
+    ;; iteratively render all characters of TEXT
+    (multiple-value-bind (width height) (text-dimensions
+                                         text
+                                         font-text-chars
+                                         :scale scale)
+      (let ((xpos (- (x-val position) (* width (x-val draw-center))))
+            (ypos (- (y-val position) (* height (y-val draw-center)))))
+        (iter (for c in-vector text)
+          (let* ((tc (@ font-text-chars c)) ;; text-char
+                 (text-char-bearing (text-char-bearing tc))
+                 (text-char-size (text-char-size tc))
+                 (text-char-advance (text-char-advance tc))
+                 ;; (tc-bearing (vec2f-mul (vec2f (cfloat (x-val text-char-bearing))
+                 ;;                               (cfloat (y-val text-char-bearing)))
+                 ;;                        scale))
+                 ;; (tc-size (vec2f-mul (vec2f (cfloat (x-val text-char-size))
+                 ;;                            (cfloat (y-val text-char-size)))
+                 ;;                     scale))
+                 ;; (tc-advance (* (x-val scale) text-char-advance))
+                 (tc-bearing (vec2f (cfloat (x-val text-char-bearing))
+                                    (cfloat (y-val text-char-bearing))))
+                 (tc-size (vec2f (cfloat (x-val text-char-size))
+                                 (cfloat (y-val text-char-size))))
+                 (tc-advance text-char-advance)
+                 (x (+ xpos (x-val tc-bearing)))
+                 (y (+ ypos (y-val tc-bearing)))
+                 (w (x-val tc-size))
+                 (h (y-val tc-size)))
 
-          ;; find height of the rendered text
-          ;; check if new lowest y position
-          (when (< ypos lowest-y)
-            (setf lowest-y ypos))
-          ;; check if new highest y position
-          (when (> (+ ypos h) highest-y)
-            (setf highest-y (+ ypos h)))
+            ;; gl rendering stuff
 
-          ;; gl rendering stuff
-          (gl:active-texture :texture0)
-          (gl:bind-texture :texture-2d (text-char-texture-id tc))
+            ;; OPTIMIZATION?
+            ;; bind TEXTURE2D unless it is already bound
+            ;; (unless (eql (id texture2d) current-texture-id)
+            ;;   (gl:active-texture :texture0)
+            ;;   (gl:bind-texture :texture-2d (id texture2d))
+            ;;   (setf current-texture-id (id texture2d)))
 
-          (gl:bind-buffer :array-buffer vbo)
-          (gl:enable-vertex-attrib-array 0)
-          (gl:vertex-attrib-pointer 0 4 :float nil (sizeof* :float 4) 0)
+            (gl:active-texture :texture0)
+            (gl:bind-texture :texture-2d (text-char-texture-id tc))
 
-          (with-sequence-to-gl-array (verts
-                                      (vector
-                                       xpos       ypos       0.0 0.0
-                                       (+ xpos w) ypos       1.0 0.0
-                                       xpos       (- ypos h) 0.0 1.0
-                                       (+ xpos w) (- ypos h) 1.0 1.0)
-                                      :float)
+            (gl:bind-buffer :array-buffer vbo)
+            (gl:enable-vertex-attrib-array 0)
+            (gl:vertex-attrib-pointer 0 4 :float nil (sizeof* :float 4) 0)
 
-            (gl:buffer-data :array-buffer :dynamic-draw verts))
-          (gl:draw-arrays :triangle-strip 0 4)
+            (with-sequence-to-gl-array (verts
+                                        (vector
+                                         x       y       0.0 0.0
+                                         (+ x w) y       1.0 0.0
+                                         x       (- y h) 0.0 1.0
+                                         (+ x w) (- y h) 1.0 1.0)
+                                        :float)
 
-          (incf (x-val position) tc-advance)
-          (incf total-width tc-advance)))
+              (gl:buffer-data :array-buffer :dynamic-draw verts))
+            (gl:draw-arrays :triangle-strip 0 4)
 
-      ;; unbind all gl stuff
-      (gl:bind-buffer :array-buffer 0)
-      (gl:bind-vertex-array 0)
-      (gl:bind-texture :texture-2d 0)
-
-      ;; width and height returned
-      (values total-width
-              (- highest-y lowest-y)))))
+            (incf xpos tc-advance))))
+      (values width height))))
 
 (defun text-width (text font-text-chars
                    &key
@@ -103,7 +140,7 @@ and scale."
   (let ((width 0.0))
     (iter (for c in-vector text)
       (let ((tc (@ font-text-chars c)))
-        (incf width (text-char-advance tc))))
+        (incf width (* (text-char-advance tc) (x-val scale)))))
     width))
 
 (defun text-height (text font-text-chars
@@ -116,14 +153,14 @@ characters and scale."
         (highest-y 0.0))
     (iter (for c in-vector text)
       (let* ((tc (@ font-text-chars c))
-             (ypos (* (y-val (text-char-bearing tc))
-                      (y-val scale)))
+             (bottom (* (y-val (text-char-bearing tc))
+                        (y-val scale)))
              (h (* (y-val (text-char-size tc))
                    (y-val scale))))
-        (when (< ypos lowest-y)
-          (setf lowest-y ypos))
-        (when (> (+ ypos h) highest-y)
-          (setf highest-y (+ ypos h)))))
+        (when (< bottom lowest-y)
+          (setf lowest-y bottom))
+        (when (> (+ bottom h) highest-y)
+          (setf highest-y (+ bottom h)))))
     (- highest-y lowest-y)))
 
 (defun text-dimensions (text font-text-chars
@@ -132,5 +169,18 @@ characters and scale."
   "=> WIDTH, HEIGHT
 Returns the width and height of the the text string with the given font
 characters and scale."
-  (values (text-width text font-text-chars :scale scale)
-          (text-height text font-text-chars :scale scale)))
+  (let ((width 0.0)
+        (lowest-y 0.0)
+        (highest-y 0.0))
+    (iter (for c in-vector text)
+      (let* ((tc (@ font-text-chars c))
+             (bottom (* (y-val (text-char-bearing tc))
+                        (y-val scale)))
+             (h (* (y-val (text-char-size tc))
+                   (y-val scale))))
+        (incf width (* (text-char-advance tc) (x-val scale)))
+        (when (< bottom lowest-y)
+          (setf lowest-y bottom))
+        (when (> (+ bottom h) highest-y)
+          (setf highest-y (+ bottom h)))))
+    (values width (- highest-y lowest-y))))
