c2323486593892ac0d24353284da3197c64c74a0
Ed Ye <hahahadude@gmail.com>
2016-01-15 06:44:10 -0500

    improve camera movement in platformer

--- examples/platformer.lisp
+++ examples/platformer.lisp
@@ -28,7 +28,7 @@
     (setf *platformer-player*
           (add-entity (map (:x 10.0)
                            (:y 10.0)
-                           (:z 1.0)
+                           (:z 0.0)
                            (:w 5.0)
                            (:h 5.0)
                            (:color (vec4f 1.0 1.0 1.0 1.0))
@@ -48,22 +48,22 @@
   (setf *platformer-level* (-> *platformer-level*
                                (with-last (map (:x 0.0)
                                                (:y 0.0)
-                                               (:z -1.0)
+                                               (:z 0.0)
                                                (:w 100.0)
                                                (:h 5.0)))
                                (with-last (map (:x 0.0)
                                                (:y 100.0)
-                                               (:z -1.0)
+                                               (:z 0.0)
                                                (:w 5.0)
                                                (:h 100.0)))
                                (with-last (map (:x 0.0)
                                                (:y 100.0)
-                                               (:z -1.0)
+                                               (:z 0.0)
                                                (:w 100.0)
                                                (:h 5.0)))
                                (with-last (map (:x 95.0)
                                                (:y 95.0)
-                                               (:z -1.0)
+                                               (:z 0.0)
                                                (:w 5.0)
                                                (:h 100.0))))))
 
@@ -137,7 +137,7 @@
         (setf result nil)))
     result))
 
-(let ((update-timer (make-timer :end (/ 1.0 70.0))))
+(let ((update-timer (make-timer :end (/ 1.0 100.0))))
   (defun platformer-update ()
     (timer-update update-timer)
 
@@ -146,78 +146,106 @@
 
       (add-event
        :code
-       (do-map (id components *entities*)
-         (let* ((x (@ components :x))
-                (y (@ components :y))
-                (w (@ components :w))
-                (h (@ components :h))
-                (velx (@ components :velx))
-                (vely (@ components :vely))
-                (max-velx (@ components :max-velx))
-                (max-vely (@ components :max-vely))
-                (gravity-p (@ components :gravity-p))
-                (accelx (@ components :accelx))
-                (accely (@ components :accely))
-                ;; (max-accelx (@ components :max-accelx))
-                ;; (max-accely (@ components :max-accely))
-                (dt (timer-end update-timer)) ;; timestep
-                (accelx/2 (* accelx 0.5 dt))
-                (accely/2 (* (- accely (if gravity-p 50.0 0.0)) 0.5 dt))
-                (face-right-p (@ components :face-right-p)))
+       (progn
+         (do-map (id components *entities*)
+           (let* ((x (@ components :x))
+                  (y (@ components :y))
+                  (w (@ components :w))
+                  (h (@ components :h))
+                  (velx (@ components :velx))
+                  (vely (@ components :vely))
+                  (max-velx (@ components :max-velx))
+                  (max-vely (@ components :max-vely))
+                  (gravity-p (@ components :gravity-p))
+                  (accelx (@ components :accelx))
+                  (accely (@ components :accely))
+                  ;; (max-accelx (@ components :max-accelx))
+                  ;; (max-accely (@ components :max-accely))
+                  (dt (timer-end update-timer)) ;; timestep
+                  (accelx/2 (* accelx 0.5 dt))
+                  (accely/2 (* (- accely (if gravity-p 50.0 0.0)) 0.5 dt))
+                  (face-right-p (@ components :face-right-p)))
 
-           ;; https://www.niksula.hut.fi/~hkankaan/Homepages/gravity.html
-           ;; symplectic euler integration
-           ;; v += a/2 * dt
-           ;; p += v * dt
-           ;; v += a/2 * dt
+             ;; https://www.niksula.hut.fi/~hkankaan/Homepages/gravity.html
+             ;; symplectic euler integration
+             ;; v += a/2 * dt
+             ;; p += v * dt
+             ;; v += a/2 * dt
 
-           (incf velx accelx/2)
-           (incf vely accely/2)
+             (incf velx accelx/2)
+             (incf vely accely/2)
 
-           ;; bound velocity once, where it affects the position
-           (setf velx (clampf velx (- max-velx) max-velx)
-                 vely (clampf vely (- max-vely) max-vely))
+             ;; bound velocity once, where it affects the position
+             (setf velx (clampf velx (- max-velx) max-velx)
+                   vely (clampf vely (- max-vely) max-vely))
 
-           (cond ((valid-move-p (+ x (* velx dt)) y w h)
-                  (incf x (* velx dt)))
-                 (t (setf velx 0.0
-                          accelx 0.0
-                          accelx/2 0.0)))
+             (cond ((valid-move-p (+ x (* velx dt)) y w h)
+                    (incf x (* velx dt)))
+                   (t (setf velx 0.0
+                            accelx 0.0
+                            accelx/2 0.0)))
 
-           (cond  ((valid-move-p x (+ y (* vely dt)) w h)
-                   (incf y (* vely dt)))
-                  (t (setf vely 0.0
-                           accely 0.0
-                           accely/2 0.0)
-                     (with! components :jump-p t)))
+             (cond  ((valid-move-p x (+ y (* vely dt)) w h)
+                     (incf y (* vely dt)))
+                    (t (setf vely 0.0
+                             accely 0.0
+                             accely/2 0.0)
+                       (with! components :jump-p t)))
 
-           (incf velx accelx/2)
-           (incf vely accely/2)
+             (incf velx accelx/2)
+             (incf vely accely/2)
+
+             (cond ((> velx 0.0) (setf face-right-p t))
+                   ((< velx 0.0) (setf face-right-p nil)))
+
+             ;; update values
+             (setf components
+                   (-> components
+                       (with :accelx accelx)
+                       (with :accely accely)
+                       (with :velx velx)
+                       (with :vely vely)
+                       (with :x x)
+                       (with :y y)
+                       (with :face-right-p face-right-p))))
+
+           ;; update the entity
+           (with! *entities* id components))
+
+         ;; update camera
+         (let* ((player (@ *entities* *platformer-player*))
+                (x (@ player :x))
+                (y (@ player :y))
+                (w (@ player :w))
+                (h (@ player :h))
+                (velx (@ player :velx))
+                (vely (@ player :vely))
+                (dt (timer-end update-timer)))
+           (with-slots (position) *camera*
+
+             ;; static
+             ;; (setf position (vec3f 50.0 0.0 100.0))
+
+             ;; choppy motion
+             ;; (setf position (vec3f (@ player :x) (@ player :y) 100.0))
+
+             ;; (incf (x-val position) (* velx dt))
+             ;; (incf (y-val position) (* vely dt))
 
 
-           (cond ((> velx 0.0) (setf face-right-p t))
-                 ((< velx 0.0) (setf face-right-p nil)))
-
-           ;; update values
-           (setf components
-                 (-> components
-                     (with :accelx accelx)
-                     (with :accely accely)
-                     (with :velx velx)
-                     (with :vely vely)
-                     (with :x x)
-                     (with :y y)
-                     (with :face-right-p face-right-p))))
-
-         ;; update the entity
-         (with! *entities* id components))))
-
-    (let ((player (@ *entities* *platformer-player*)))
-      (with-slots (position) *camera*
-        (setf position (vec3f (+ (@ player :x) (/ (@ player :w) 2.0))
-                              (+ (@ player :y) (/ (@ player :h) 2.0))
-                              70.0))
-        (update-camera-vectors *camera*)))
+             ;; better, faster camera movement causes camera to be stable quicker
+             ;; less stuttering of the camera
+             (let* ((x-forwardness ;;(* velx 1.3)
+                      (* (signum velx) w 3.0))
+                    (y-forwardness (* vely 1.3))
+                    (x-diff (+ (- (+ x (/ w 2.0)) (x-val position))
+                               x-forwardness))
+                    (y-diff (+ (- (- y (/ h 2.0)) (y-val position))))
+                    (change 2.0))
+               (incf (x-val position) (* change x-diff dt))
+               (incf (y-val position) (* change y-diff dt)))
+             (setf (z-val position) 100.0)
+             (update-camera-vectors *camera*))))))
 
     ;; update view matrices based on camera
     (let ((cube-program (get-program "cube"))
@@ -260,7 +288,8 @@
                    :rotate (vec3f 0.0 (if face-right-p (cfloat pi) 0.0) 0.0)
                    :draw-center (vec3f -0.5 0.5 0.0)
                    :clip-position (vec2f 11.0 128.0)
-                   :clip-size (vec2f 45.0 62.0)))))
+                   :clip-size (vec2f 45.0 62.0))
+      )))
 
 (defun platformer-render-level ()
   (do-seq (components *platformer-level*)
@@ -269,7 +298,7 @@
                :color (vec4f 0.0 0.5 0.2 1.0)
                :draw-center (vec3f -0.5 0.5 0.0))))
 
-(let ((render-timer (make-timer :end (/ 1.0 60.0))))
+(let ((render-timer (make-timer :end (/ 1.0 65.0))))
   (defun platformer-render ()
     (timer-update render-timer)
     (when (timer-ended-p render-timer)
@@ -290,10 +319,12 @@
                        (@ player :x) (@ player :y)
                        (@ player :velx) (@ player :vely)
                        (@ player :accelx) (@ player :accely)))
-             (object-in-view (kit.glm:matrix*vec4
-                              (kit.glm:matrix* (get-view-matrix *camera*)
-                                               (kit.glm:translate* 0.0 0.0 -1.0))
-                              (vec4f 0.0 0.0 -1.0 1.0))))
+             ;; trying to figure out if object is in camera view
+             ;; (object-in-view (kit.glm:matrix*vec4
+             ;;                  (kit.glm:matrix* (get-view-matrix *camera*)
+             ;;                                   (kit.glm:translate* 0.0 0.0 -1.0))
+             ;;                  (vec4f 0.0 0.0 -1.0 1.0)))
+             )
         ;; (cube-draw :position (vec3f (@ player :x) (@ player :y) 0.0)
         ;;            :size (vec3f 1.0 1.0 1.0)
         ;;            :color (vec4f 0.0 0.0 1.0 1.0)
@@ -309,13 +340,15 @@
                      :draw-center (vec3f -0.5 -0.5 0.0)
                      :position (vec3f (cfloat (- *width* w)) (cfloat (- *height* h)) 0.0)
                      :scale text-scale))
-        (text-draw (format nil "~a ~a ~a"
-                           (x-val object-in-view)
-                           (y-val object-in-view)
-                           (z-val object-in-view))
-                   (get-font "sans24")
-                   :position (vec2f 0.0 100.0)
-                   :draw-center (vec3f -0.5 -0.5 0.0)))
+
+        ;; (text-draw (format nil "~a ~a ~a"
+        ;;                    (x-val object-in-view)
+        ;;                    (y-val object-in-view)
+        ;;                    (z-val object-in-view))
+        ;;            (get-font "sans24")
+        ;;            :position (vec2f 0.0 100.0)
+        ;;            :draw-center (vec3f -0.5 -0.5 0.0))
+        )
 
       ;; fps
       (text-draw (format nil "~4f" (cfloat (average-fps)))
