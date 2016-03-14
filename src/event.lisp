f0e3456a1296b1784222014a23df3e9687c51d9b
Ed Ye <hahahadude@gmail.com>
2016-03-14 18:48:12 -0400

    Handle events that add events.

--- src/event.lisp
+++ src/event.lisp
@@ -5,9 +5,19 @@
 (in-package :err)
 
 (defmacro add-event (&key code)
-  `(alexandria:appendf *destructive-changes* (list (lambda () ,code))))
+  `(vector-push-extend (lambda () ,code) *destructive-changes*))
+
+(defmacro defevent (name parameters &body body)
+  `(defun ,name ,parameters
+     (add-event :code (progn ,@body))))
 
 (defun update-events ()
-  (mapcar #'funcall *destructive-changes*)
-  (setf *destructive-changes* nil)
+  (let ((counter 0)
+        (len (length *destructive-changes*)))
+    (iter (while (> len counter))
+      (iter (while (< counter len))
+        (funcall (aref *destructive-changes* counter))
+        (incf counter))
+      (setf len (length *destructive-changes*)))
+    (setf *destructive-changes* (make-array 0 :adjustable t :fill-pointer 0 :element-type 'function)))
   t)
